e3380ac6caa1d13cd18e1452884244e956084ecf
Stewart Kelly <stewkelly@icloud.com>
2024-10-09 10:45:56 +1100

    plyfiles

--- Code/plotEnvironment.m
+++ Code/plotEnvironment.m
@@ -2,7 +2,7 @@ function plotEnvironment()
     hold on;
     axis tight
     obj1 = PlaceObject('bread.ply',[0.5,0,1]);
-    obj1 = PlaceObject('toast.ply',[-0.5,0,1]);
+    obj2 = PlaceObject('toast.ply',[-0.5,0,1]);
     obj3 = PlaceObject('table.ply',[0,0,0]);
 
 end
