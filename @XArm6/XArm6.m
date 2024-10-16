0b2a399f10ed345654c375c9dc86bb0961de6696
Stewart Kelly <stewkelly@icloud.com>
2024-10-16 13:33:28 +1100

    Fixed XArm6 Ply and DH - all working

--- @XArm6/XArm6.m
+++ @XArm6/XArm6.m
@@ -1,7 +1,7 @@
 classdef XArm6 < RobotBaseClass
    %%
     properties(Access = public)              
-        plyFileNameStem = 'Xarm6';
+        plyFileNameStem = 'XArm6';
     end
     
     methods
@@ -14,65 +14,23 @@ classdef XArm6 < RobotBaseClass
             end
             self.model.base = self.model.base.T;
             
-            
-            % Call the overridden PlotAndColourRobot function
             self.PlotAndColourRobot();         
         end
 
         %% Create the robot model
         function CreateModel(self)   
-            % Create the Xarm6 model mounted on a linear rail
+            % Create the Xarm6 model 
            
-            link(1) = Link('d',0.267,'a',0,'alpha',0,'qlim',[2*pi, 2*pi], 'offset',0);
-            link(2) = Link('d',0,'a',0,'alpha',0,'qlim',[2*pi, 2*pi], 'offset',pi);
-            link(3) = Link('d',0.2805,'a',-0.051,'alpha',0,'qlim',[2*pi, 2*pi], 'offset',0);
-            link(4) = Link('d',-0.3425,'a',0.0775,'alpha',pi,'qlim',[2*pi, 2*pi], 'offset',pi);
-            link(5) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[2*pi, 2*pi], 'offset',0);
-            link(6) = Link('d',0.1,'a',0.1,'alpha',pi/2,'qlim',[2*pi, 2*pi], 'offset',pi/2);
+            link(1) = Link('d',0.267,'a',0,'alpha',-pi/2,'qlim',[-2*pi, 2*pi], 'offset',0);
+            link(2) = Link('d',0,'a',0.2895,'alpha',0,'qlim',[-deg2rad(118), deg2rad(120)], 'offset',deg2rad(-79));
+            link(3) = Link('d',0,'a',0.0775,'alpha',-pi/2,'qlim',[-deg2rad(225), deg2rad(11)], 'offset',deg2rad(79));
+            link(4) = Link('d',0.3425,'a',0,'alpha',pi/2,'qlim',[-2*pi, 2*pi], 'offset',0);
+            link(5) = Link('d',0,'a',0.076,'alpha',-pi/2,'qlim',[-deg2rad(97), pi], 'offset',0);
+            link(6) = Link('d',0.097,'a',0,'alpha',0,'qlim',[-2*pi, 2*pi], 'offset',0);
 
       
             self.model = SerialLink(link,'name',self.name);
         end
-     
-        %% Override PlotAndColourRobot to assign red color
-        function PlotAndColourRobot(self)
-            % Call the base class method to load .ply data
-            PlotAndColourRobot@RobotBaseClass(self);
-
-            % Set all vertices to red
-            for linkIndex = 0:self.model.n
-                % Set the default colour to red (R=1, G=0, B=0)
-                vertexColours = repmat([0.5, 0.5, 0.5], size(self.model.points{linkIndex+1}, 1), 1); % Red color
-                
-                % Assign the red colour to the plot
-                h = findobj('Tag', self.model.name);
-                h = get(h, 'UserData');
-                h.link(linkIndex+1).Children.FaceVertexCData = vertexColours;
-                h.link(linkIndex+1).Children.FaceColor = 'interp'; % Interpolated shading
-            end
-            drawnow();
-        end
     end
 end
 
-
-%{
-
- link(1) = Link('d',0,'a',0,'alpha',0,'qlim',0, 'offset',0);
-            link(2) = Link('d',0.267,'a',0,'alpha',0,'qlim',0, 'offset',0);
-            link(3) = Link('d',0,'a',0,'alpha',-pi/2,'qlim',0, 'offset',deg2rad(-79.34995));
-            link(4) = Link('d',0,'a',0.28948866,'alpha',0,'qlim',0, 'offset',deg2rad(79.34995));
-            link(5) = Link('d',0.3425,'a',0.0775,'alpha',-pi/2,'qlim',0, 'offset',0);
-            link(6) = Link('d',0,'a',0,'alpha',pi/2,'qlim',0, 'offset',0);
-            link(7) = Link('d',0.097,'a',0.076,'alpha',-pi\2,'qlim',0, 'offset',0);
-      
-
-link(1) = Link('d',0,'a',0,'alpha',0,'qlim',0, 'offset',0);
-            link(2) = Link('d',0,'a',0,'alpha',0,'qlim',0, 'offset',0);
-            link(3) = Link('d',0,'a',0,'alpha',0,'qlim',0, 'offset',0);
-            link(4) = Link('d',0,'a',0,'alpha',0,'qlim',0, 'offset',0);
-            link(5) = Link('d',0,'a',0,'alpha',0,'qlim',0, 'offset',0);
-            link(6) = Link('d',0,'a',0,'alpha',0,'qlim',0, 'offset',0);
-            link(7) = Link('d',0,'a',0,'alpha',0,'qlim',0, 'offset',0);
-
-%}
--- Code/Test.m
+++ Code/Test.m
@@ -1,13 +1,15 @@
 function Test()
-    axis equal;
-    r = DobotCR10(transl(0,0,1)*troty(-pi/2));
-    
-    qMatrix = jtraj(r.model.qlim(:,1),r.model.qlim(:,2),100);
+axis padded;
+clf;
 
-    for i = 1:100
-        r.model.animate(qMatrix(i,:));
-        drawnow;
-        pause(0.1);
-    end
+r = XArm6();
+
+qMatrix = jtraj(r.model.qlim(:,1),r.model.qlim(:,2),100);
+
+for i = 1:100
+    r.model.animate(qMatrix(i,:));
+    drawnow;
+    pause(0.1);
+end
 
 end
